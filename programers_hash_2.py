1a48591680ec53cb47b42d1d90b6d381f7ab70a8
chiriem <maoru23@gmail.com>
2022-03-25 11:02:24 +0900

    number2

---
+++ programers_hash_2.py
@@ -0,0 +1,17 @@
+def solution(phone_book):
+    phone_book.sort()
+
+    answer = True
+
+    phone_length = len(phone_book)
+
+    for i in range(phone_length - 1):
+
+        if phone_book[i] == phone_book[i + 1][0:len(phone_book[i])]:
+            answer = False
+
+            break
+
+    answer = hash(phone_book[2])
+
+    return answer
