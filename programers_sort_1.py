3ac4a932691ef59033c6e82f20dc3a1a4296af55
chiriem <maoru23@gmail.com>
2022-04-04 08:51:55 +0900

    prog python 2

---
+++ programers_sort_1.py
@@ -0,0 +1,19 @@
+def solution(array, commands):
+    answer = []
+
+    cmd_length = len(commands)
+
+    for i in range(cmd_length):
+        arr_list = array[commands[i][0]-1:commands[i][1]]
+
+        arr_list.sort()
+
+        answer.append(arr_list[commands[i][2]-1])
+
+    return answer
+
+if __name__ == '__main__':
+    array = [1, 5, 2, 6, 3, 7, 4]
+    commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+    print(solution(array, commands))
