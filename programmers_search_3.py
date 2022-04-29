6e793e3872af5a72badb18ea71accc8973ad146f
chiriem <maoru23@gmail.com>
2022-04-29 10:28:02 +0900

    python search++

--- programmers_search_3.py
+++ programmers_search_3.py
@@ -1,16 +1,14 @@
 def solution(brown, yellow):
-
     ab = brown + yellow
 
-    for b in range(1, ab+1):
+    for b in range(1, ab + 1):
         if ab % b == 0:
             a = int(ab / b)
             if a >= b:
-                if 2*a + 2*b == brown + 4:
+                if 2 * a + 2 * b == brown + 4:
                     answer = [a, b]
 
                     return answer
 
-b = 10
-y = 2
+
 print(solution(10, 2))
